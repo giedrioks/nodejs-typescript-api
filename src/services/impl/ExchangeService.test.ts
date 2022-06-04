a29496d3cac51a37eac6d1f615ed56ed4978e625
Giedrius Valintelis <giedrius.valintelis@lt.ibm.com>
2022-06-04 20:34:00 +0300

    Adding LRU cache and Exchange service tests.

---
+++ src/services/impl/ExchangeService.test.ts
@@ -0,0 +1,68 @@
+import { ExchangeService } from "./ExchangeService";
+import { IExchangeRateProvider } from "../IExchangeRateProvider";
+
+describe("Exchange service tests", () => {
+  test("Calculates proper amount when rate provider returns successfully", async () => {
+    const mockedProvider = {
+      get: async () => Promise.resolve({ rates: { EUR: 1.23, GBP: 1.01 } }),
+    } as IExchangeRateProvider;
+
+    const service = new ExchangeService(mockedProvider);
+    const result = await service.exchange({
+      from: "USD",
+      to: "EUR",
+      amount: 100,
+    });
+
+    expect(result.amount).toBe(123);
+  });
+
+  test("Returns rate from rate provider with amount", async () => {
+    const mockedProvider = {
+      get: async () => Promise.resolve({ rates: { EUR: 1.23, GBP: 1.01 } }),
+    } as IExchangeRateProvider;
+
+    const service = new ExchangeService(mockedProvider);
+    const result = await service.exchange({
+      from: "USD",
+      to: "EUR",
+      amount: 100,
+    });
+
+    expect(result.rate).toBe(1.23);
+  });
+
+  test("Throws exception, when rate provider internally trows exception", async () => {
+    const mockedProvider = {
+      get: async () => {
+        throw new Error("Server unavailable");
+      },
+    } as IExchangeRateProvider;
+
+    const service = new ExchangeService(mockedProvider);
+
+    await expect(
+      service.exchange({
+        from: "USD",
+        to: "EUR",
+        amount: 100,
+      })
+    ).rejects.toEqual(new Error("Server unavailable"));
+  });
+
+  test("Throws exception, when rate provider returns empty result", async () => {
+    const mockedProvider = {
+      get: async () => Promise.resolve(),
+    } as IExchangeRateProvider;
+
+    const service = new ExchangeService(mockedProvider);
+
+    await expect(
+      service.exchange({
+        from: "USD",
+        to: "EUR",
+        amount: 100,
+      })
+    ).rejects.toEqual(new Error("Unable to retrieve rates for currency [EUR]"));
+  });
+});
---
+++ src/services/impl/LruCacheService.test.ts
@@ -0,0 +1,65 @@
+import { LruCacheService } from "./LruCacheService";
+
+describe("LRU Cache tests", () => {
+  const KEY1_VALUE = { v: 1 };
+  const KEY2_VALUE = { v: 2 };
+  const KEY3_VALUE = { v: 3 };
+  const KEY4_VALUE = { v: 4 };
+
+  const KEY1 = "key1";
+  const KEY2 = "key2";
+  const KEY3 = "key3";
+  const KEY4 = "key4";
+
+  test("Cache instance created with provided max entries", () => {
+    const cache = new LruCacheService(1);
+    expect(cache.maxEntries).toBe(1);
+  });
+
+  test("Cache instance created with default max entries = 5", () => {
+    const cache = new LruCacheService();
+    expect(cache.maxEntries).toBe(5);
+  });
+
+  test("Cache stores only max entries even adding more", () => {
+    const cache = new LruCacheService(1);
+    cache.put(KEY1, KEY1_VALUE);
+    cache.put(KEY2, KEY2_VALUE);
+    cache.put(KEY3, KEY3_VALUE);
+    expect(cache.values.size).toBe(1);
+  });
+
+  test("Cache evicts first added entry", () => {
+    const cache = new LruCacheService(2);
+    cache.put(KEY1, KEY1_VALUE);
+    cache.put(KEY2, KEY2_VALUE);
+    cache.put(KEY3, KEY3_VALUE);
+
+    expect(cache.get(KEY1)).toBe(undefined);
+  });
+
+  test("Cache evicts least used entry", () => {
+    const cache = new LruCacheService(2);
+    cache.put(KEY1, KEY1_VALUE);
+    cache.put(KEY2, KEY2_VALUE);
+    cache.put(KEY3, KEY3_VALUE);
+
+    cache.get(KEY2);
+    cache.get(KEY1);
+    cache.put(KEY4, KEY4_VALUE); // KEY3 remains least used before adding extra, so will be evicted
+
+    expect(cache.get(KEY3)).toBe(undefined);
+  });
+
+  test("has() returns true if key exists", () => {
+    const cache = new LruCacheService(1);
+    cache.put(KEY1, KEY1_VALUE);
+    expect(cache.has(KEY1)).toBe(true);
+  });
+
+  test("has() returns false if key not exists", () => {
+    const cache = new LruCacheService(1);
+    cache.put(KEY1, KEY1_VALUE);
+    expect(cache.has(KEY2)).toBe(false);
+  });
+});
